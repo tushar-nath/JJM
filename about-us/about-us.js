80df1841885e0697b5fe82515bd7c2ddff6747c2
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2022-08-20 14:08:44 +0530

    about-us updated

--- about-us/about-us.js
+++ about-us/about-us.js
@@ -26,3 +26,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
