44a1c9eab9e4ba8ee3a3676e3c87ff6e0465c7cb
tusharnath10 <tusharnath10@gmail.com>
2022-08-22 00:42:43 +0530

    Dashboard Page Updated the dashboard page and the vertical carousel. Also, changed a few attributes in the JSON file.

---
+++ map-view-screen/data.js
@@ -0,0 +1,38 @@
+var markers = [
+    {
+        "title": 'Dhekiajuli',
+        "lat": '26.6985',
+        "lng": '92.4851',
+        "total": '1500',
+        "active": '1250',
+        "iot": '1000',
+        "upcoming": '237'
+    },
+    {
+        "title": 'Bihaguri',
+        "lat": '26.6744',
+        "lng": '92.6810',
+        "total": '1500',
+        "active": '1250',
+        "iot": '1000',
+        "upcoming": '237'
+    },
+    {
+        "title": 'Tezpur',
+        "lat": '26.6528',
+        "lng": '92.7926',
+        "total": '1500',
+        "active": '1250',
+        "iot": '1000',
+        "upcoming": '237'
+    },
+    {
+        "title": 'Chutia',
+        "lat": '26.7347',
+        "lng": '93.0463',
+        "total": '1500',
+        "active": '1250',
+        "iot": '1000',
+        "upcoming": '237'
+    }
+];
