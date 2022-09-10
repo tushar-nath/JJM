f6c27de0755a6965ff04ccc8e564b9ea6154f374
Moloy Writ Saikia <46087868+writ-tech@users.noreply.github.com>
2022-09-11 04:07:47 +0530

    Update bar-graph.js

--- graph_page6/bar-graph.js
+++ graph_page6/bar-graph.js
@@ -55,7 +55,7 @@ async function dummyChart() {
 					ticks: { min: 0 },
 					scaleLabel: {
 						display: true,
-						labelString: 'pH Level'
+						labelString: 'Water Level'
 					}
 				}],
 			},
